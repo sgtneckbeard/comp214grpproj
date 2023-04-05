8503c81acf1bc26a888784455abd020cffba15a0
Yusuf Esad Bilgic <91512750+Yuseb@users.noreply.github.com>
2023-04-05 00:16:16 -0400

    Update Package.sql

--- Package.sql
+++ Package.sql
@@ -1,50 +1,3 @@
-CREATE OR REPLACE PACKAGE BODY user_auth_pkg AS
-    -- Function to check if a given username and password match a user in the MM_Users table
-    FUNCTION authenticate_user(p_username IN VARCHAR2, p_password IN VARCHAR2) RETURN BOOLEAN AS
-        v_user_count NUMBER;
-    BEGIN
-        SELECT COUNT(*) INTO v_user_count
-        FROM MM_Users
-        WHERE username = p_username AND password = p_password;
-        
-        IF v_user_count > 0 THEN
-            RETURN TRUE;
-        ELSE
-            RETURN FALSE;
-        END IF;
-    END authenticate_user;
-    
-    -- Procedure to register a new user
-    PROCEDURE register_user(p_username IN VARCHAR2, p_password IN VARCHAR2, p_email IN VARCHAR2) AS
-    BEGIN
-        INSERT INTO MM_Users (user_id, username, password, email)
-        VALUES (user_id_seq.NEXTVAL, p_username, p_password, p_email);
-    END register_user;
-    
-    -- Procedure to reset a user's password
-    PROCEDURE reset_password(p_username IN VARCHAR2, p_new_password IN VARCHAR2) AS
-    BEGIN
-        UPDATE MM_Users
-        SET password = p_new_password
-        WHERE username = p_username;
-    END reset_password;
-    
-END user_auth_pkg;
-/
-
-
--- Register a new user
-BEGIN
-   user_auth_pkg.register_user('johndoe', 'password123', 'johndoe@example.com');
- 	DBMS_OUTPUT.PUT_LINE('Register successful');
-   COMMIT;
-
-END;
-/
-
------------------------------
-
-
 CREATE OR REPLACE PACKAGE user_auth_pkg AS
     -- Function to check if a given username and password match a user in the MM_Users table
     FUNCTION authenticate_user(p_username IN VARCHAR2, p_password IN VARCHAR2) RETURN BOOLEAN;
@@ -158,13 +111,53 @@ BEGIN
 END;
 
 
+-----------------------------------------------
 
 
 
+CREATE OR REPLACE PACKAGE BODY user_auth_pkg AS
+    -- Function to check if a given username and password match a user in the MM_Users table
+    FUNCTION authenticate_user(p_username IN VARCHAR2, p_password IN VARCHAR2) RETURN BOOLEAN AS
+        v_user_count NUMBER;
+    BEGIN
+        SELECT COUNT(*) INTO v_user_count
+        FROM MM_Users
+        WHERE username = p_username AND password = p_password;
+        
+        IF v_user_count > 0 THEN
+            RETURN TRUE;
+        ELSE
+            RETURN FALSE;
+        END IF;
+    END authenticate_user;
+    
+    -- Procedure to register a new user
+    PROCEDURE register_user(p_username IN VARCHAR2, p_password IN VARCHAR2, p_email IN VARCHAR2) AS
+    BEGIN
+        INSERT INTO MM_Users (user_id, username, password, email)
+        VALUES (user_id_seq.NEXTVAL, p_username, p_password, p_email);
+    END register_user;
+    
+    -- Procedure to reset a user's password
+    PROCEDURE reset_password(p_username IN VARCHAR2, p_new_password IN VARCHAR2) AS
+    BEGIN
+        UPDATE MM_Users
+        SET password = p_new_password
+        WHERE username = p_username;
+    END reset_password;
+    
+END user_auth_pkg;
+/
 
 
+-- Register a new user
+BEGIN
+   user_auth_pkg.register_user('johndoe', 'password123', 'johndoe@example.com');
+ 	DBMS_OUTPUT.PUT_LINE('Register successful');
+   COMMIT;
 
-
+END;
+/
 
 
 
